8dc574756a8febbbe6cb615322b9bcbb6e4d90e3
Serkan Erip <serkanerip@gmail.com>
2020-09-27 02:54:54 +0300

    improvements on tags finder.

--- backend/app/business/tags_finder.py
+++ backend/app/business/tags_finder.py
@@ -1,4 +1,4 @@
-def exportTagsFromText(text, allTags):
+def exportTagsFromTextOld(text, allTags):
     exportedTags = []
     text = text.lower()
     text = remove_html_tags(text)
@@ -8,6 +8,31 @@ def exportTagsFromText(text, allTags):
     textArr = set(text.split(' '))
     for tag in allTags:
         if tag in textArr:
+            exportedTags.append(tag)
+    return exportedTags
+
+def exportTagsFromText(text, allTags):
+    exportedTags = []
+    text = text.lower()
+    text = remove_html_tags(text)
+    
+    if not 'reactjs' in text:
+        text = text.replace('react', 'reactjs')
+    if not 'vue.js' in text:
+        text = text.replace('vue', 'vue.js')
+        text = text.replace('vuejs', 'vue.js')
+
+    replaceChars = ",({)}"
+    for char in replaceChars:
+        text = text.replace(char, ' ')
+    for tag in allTags:
+        tag = tag.replace('-', ' ')
+        pos = text.find(tag)
+        prevChar = (text[pos-1] is ' ' or text[pos-1] is ',' or text[pos-1] is '\n')
+        nextChar = (text[pos+len(tag)] is ' ' or text[pos+len(tag)] is ',' or text[pos+len(tag)] is '\n')
+        if tag == "git":
+            print(pos, text[pos-1],prevChar, nextChar)
+        if pos != -1 and nextChar and prevChar:
             exportedTags.append(tag)
     return exportedTags
 
